2b9e53d91eca6be30ecf4aa9f4cafe6b9a8c8960
Paulo Marques Vieira <marquespv@yahoo.com.br>
2020-11-20 11:10:43 -0300

    Atualizacao

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO client VALUES (1, 'Araujo Santana', '73245634590', 'Rua Joquim Nabuco, 536');
+INSERT INTO client VALUES (2, 'Lucia Monteiro', '19264934580', 'Rua Conselheiro Weber, 99');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS client (
+  id              INT     NOT NULL PRIMARY KEY,
+  nome       	  VARCHAR(200) NOT NULL,
+  cpf             VARCHAR(200) NOT NULL,
+  endereco       VARCHAR(200) NOT NULL  
+);
